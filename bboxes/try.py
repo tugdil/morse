a76cdf8859d53c7f61748832d5ae32ac8bd8d48d
tugdil <b._matthias@hotmail.de>
2020-06-25 10:00:14 +0200

    detecting overlaps (multiple objects in one bounding box)

--- bboxes/try.py
+++ bboxes/try.py
@@ -13,12 +13,37 @@ images_path = os.path.join(base_path, 'images')
 out_path = os.path.join(images_path, this_time)
 os.mkdir(out_path)
 idx = 0
+meta_data = {}
+labels = ['index', 'image_path', 'object_class', 'object_name', 'overlaps']
 
 
 def send_destination(s, simu, x, y, yaw):
     s.publish({'x': x, 'y': y, 'z': 0, 'yaw': yaw, 'pitch': 0.0, 'roll': 0.0})
     simu.sleep(0.5)
 
+def overlaps(bbox_coordinates):
+    overlaps = {}
+    for index in list(bbox_coordinates):
+        overlaps[index] = []
+    copy = bbox_coordinates.copy()
+    for index in list(bbox_coordinates):
+        copy.pop(index)
+        for bbox in list(copy):
+            if is_overlapping(bbox_coordinates[index], copy[bbox]):
+                overlaps[index].append(bbox)
+                overlaps[bbox].append(index)
+    return overlaps
+
+def is_overlapping(coords_1, coords_2):
+    if coords_1[0] > coords_2[1]:
+        return False
+    if coords_1[1] < coords_2[0]:
+        return False
+    if coords_1[2] > coords_2[3]:
+        return False
+    if coords_1[3] < coords_2[2]:
+        return False
+    return True
 
 with Morse() as morse:
     semantic_camera = morse.robot.semantic_camera
@@ -44,7 +69,11 @@ with Morse() as morse:
                 image_bytes = base64.b64decode(image_data)
                 image = Image.frombytes('RGBA', (int(height), int(width)), image_bytes)
 
+                bbox_coordinates = {}
+
                 for visible_object in semantic_camera_data['visible_objects']:
+                    # get ambiguous view
+                    # get bounding box image
                     bbox = visible_object['bbox']
                     x_min = width
                     x_max = 0
@@ -70,12 +99,24 @@ with Morse() as morse:
                     x_max = math.ceil(x_max)
                     y_min = height - math.floor(y_min)
                     y_max = height - math.ceil(y_max)
-                    print(''+str(x_min)+', '+str(x_max))
+                    bbox_coordinates[idx] = [x_min, x_max, y_max, y_min]
                     img = image.crop((x_min, y_max, x_max, y_min))
-                    img_name = str(idx) + '_' + visible_object['name'] + '.png'
+                    img_name = str(idx) + '.png'
                     image_path = os.path.join(out_path, img_name)
                     img.save(image_path)
                     object_category = visible_object['name'].split('.')[0]
+                    meta_data[idx] = [idx, image_path, object_category, visible_object['name']]
                     meta_writer.writerow([idx, image_path, object_category, visible_object['name']])
                     idx += 1
                 counter += 1
+
+                overlaps = overlaps(bbox_coordinates)
+                for row in list(meta_data):
+                    meta_data[row].append(len(overlaps[row]))
+
+    with open(os.path.join(out_path, 'meta_data_test.csv'), mode='w') as meta_file:
+        meta_writer = csv.writer(meta_file)
+        meta_writer.writerow(labels)
+        for i in range(len(list(meta_data))):
+            meta_writer.writerow(meta_data[i])
+
